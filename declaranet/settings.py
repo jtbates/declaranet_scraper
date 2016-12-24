ec15d6ccfbeed8090accae05316a6e6ebc843837
Jordan T. Bates <jtbates@gmail.com>
2016-12-23 18:33:49 -0600

    Add sqlalchemy and alembic

--- declaranet/settings.py
+++ declaranet/settings.py
@@ -10,3 +10,14 @@ config.read(config_path)
 main_cfg = config['declaranet']
 
 
+def rel2abs(path, base_dir=BASE_DIR):
+    if os.path.isabs(path):
+        return(path)
+    else:
+        return(os.path.join(BASE_DIR, path))
+
+
+SQLITE_DB_PATH = rel2abs(main_cfg['sqlite_db_path'])
+DB_URI = "sqlite:///{}".format(SQLITE_DB_PATH)
+ALEMBIC_INI_PATH = rel2abs(main_cfg['alembic_ini_path'])
+ALEMBIC_SCRIPT_LOCATION = rel2abs(main_cfg['alembic_script_location'])
