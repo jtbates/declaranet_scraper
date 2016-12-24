3ffca9a8174e734833e693099e9d9f231615f000
Jordan T. Bates <jtbates@gmail.com>
2016-12-23 18:34:07 -0600

    Add logging configuration

--- declaranet/settings.py
+++ declaranet/settings.py
@@ -1,4 +1,6 @@
 import configparser
+import logging
+from logging.config import fileConfig
 import os
 
 
@@ -21,3 +23,10 @@ SQLITE_DB_PATH = rel2abs(main_cfg['sqlite_db_path'])
 DB_URI = "sqlite:///{}".format(SQLITE_DB_PATH)
 ALEMBIC_INI_PATH = rel2abs(main_cfg['alembic_ini_path'])
 ALEMBIC_SCRIPT_LOCATION = rel2abs(main_cfg['alembic_script_location'])
+
+
+# Set up logging
+fileConfig(config_path)
+default_fmt = config['formatter_default'].get('format', raw=True)
+default_datefmt = config['formatter_default'].get('datefmt', raw=True)
+default_formatter = logging.Formatter(default_fmt, default_datefmt)
